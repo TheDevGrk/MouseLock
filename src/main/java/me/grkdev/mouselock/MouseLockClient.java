93000e9cfbf79bed25ffac20d933ce63fee42d0b
GrkDev <thedevgrk@gmail.com>
2024-11-10 22:57:31 -0600

    Init

---
+++ src/main/java/me/grkdev/mouselock/MouseLockClient.java
@@ -0,0 +1,10 @@
+package me.grkdev.mouselock;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MouseLockClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
